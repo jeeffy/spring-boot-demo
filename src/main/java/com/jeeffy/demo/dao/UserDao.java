29add77b66b345ee71e910d796a59d573433fddd
jiangfeng <jiangfeng@126.com>
2017-07-26 09:00:25 +0800

    add method

--- src/main/java/com/jeeffy/demo/dao/UserDao.java
+++ src/main/java/com/jeeffy/demo/dao/UserDao.java
@@ -1,14 +1,11 @@
 package com.jeeffy.demo.dao;
 
+import com.jeeffy.demo.bean.User;
 import com.jeeffy.demo.bean.User2;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jeeffy.demo.bean.User;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
 
@@ -16,8 +13,8 @@ public interface UserDao extends JpaRepository<User, Integer>{
 
     User findByUsername(String username);
 
-    /*@Query("select u.id as id, u.username as username from User u where u.deptId = :deptId and id<112")
-    List<User2> findByDeptId(@Param("deptId") Integer deptId);*/
+    @Query("select u.id as id, u.username as username from User u where u.deptId = :deptId and id<112")
+    List<User2> findByDeptId2(@Param("deptId") Integer deptId);
 
     @Query("select u.id as id, u.username as username from User u where u.deptId = :deptId and id<112")
     List<Map> findByDeptId(@Param("deptId") Integer deptId);
